42c05690783510be869b9fcb6b0254db71dcdaf9
DragonFruit8 <Jtbyers19@gmail.com>
2025-07-21 16:23:23 -0400

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,7 +2,6 @@ import TodoListItem from './TodoListItem';
 import todos from './todolist.json';
 
 function TodoList () {
-  // Mentor requested for me to list todo items from JSON file
   const todo = todos.todo.map (item => {
     return <TodoListItem key={item.id} title={item.item} />;
   });
