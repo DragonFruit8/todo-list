39d7fbf76cb8f382a93a73be4b9463b0e9ed8a45
DragonFruit8 <Jtbyers19@gmail.com>
2025-07-30 06:01:46 -0400

    feat: API Fetch - Airtable

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import {useState} from 'react';
-import TodoList from './features/TodoList/TodoList';
-import TodoForm from './features/TodoForm';
-import './App.css';
+import { useState, useEffect } from "react";
+import TodoList from "./features/TodoList/TodoList";
+import TodoForm from "./features/TodoForm";
+import "./App.css";
 
-function App () {
-  const [todoList, setTodoList] = useState ([]);
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${
+    import.meta.env.VITE_TABLE_NAME
+  }`;
+  const token = import.meta.env.VITE_PAT
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function addTodo (title) {
+  function addTodo(title) {
     const newTodo = {
-      id: Date.now (),
+      id: Date.now(),
       title,
       isCompleted: false,
     };
-    setTodoList ([...todoList, newTodo]);
+    setTodoList([...todoList, newTodo]);
   }
 
-  function completeTodo (id) {
-    const updatedTodos = todoList.map (todo => {
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
       if (todo.id === id) {
-        return {...todo, isCompleted: true};
+        return { ...todo, isCompleted: true };
       }
       return todo;
     });
-    setTodoList (updatedTodos);
+    setTodoList(updatedTodos);
   }
 
-  function updateTodo (editedTodo) {
-    const updatedTodo = todoList.map (todo => {
+  function updateTodo(editedTodo) {
+    const updatedTodo = todoList.map((todo) => {
       if (todo.title !== editedTodo) {
         return {
           id: todo.id,
@@ -37,18 +43,56 @@ function App () {
       return editedTodo;
     });
 
-    setTodoList (updatedTodo);
+    setTodoList(updatedTodo);
   }
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      body: JSON.stringify(),
+      headers: {  
+        Authorization: `Bearer ${token}` },
+    };
+    const fetchTodos = async () => {
+      try {
+        setIsLoading(true);
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          setErrorMessage("Error: " + resp.status);
+          throw new Error(resp.status);
+        }
+        const data = await resp.json();
+        if (data.status != "success") {
+          setErrorMessage("Status: " + resp.status);
+        }
+        setTodoList({
+          id: data.records.id,
+          title: data.records.title,
+          isCompleted: data.records.isCompleted
+        })
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, [url, token]);
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList
-        todoList={todoList}
-        onUpdateTodo={updateTodo}
-        onCompleteTodo={completeTodo}
-      />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList
+          todoList={todoList}
+          onUpdateTodo={updateTodo}
+          onCompleteTodo={completeTodo}
+        />
+      )}
+      {errorMessage}
     </div>
   );
 }
